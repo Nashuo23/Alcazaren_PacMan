76f16df10849afb8b72aa1358fad0a2100e5680b
Nashuo23 <1chomacalcazaren@gmail.com>
2025-10-28 21:46:44 +1100

    Fixing Tween

--- Assets/Scripts/Level1/Tween.cs
+++ Assets/Scripts/Level1/Tween.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tween : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Tween {
+    public Transform Target { get; private set; }
+    public Vector3 StartPos { get; private set; }
+    public Vector3 EndPos { get; private set; }
+    public float StartTime { get; private set; }
+    public float Duration { get; private set; }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public Tween(Transform target, Vector3 origin, Vector3 destination, float startTime, float duration) {
+        Target = target;
+        StartPos = origin;
+        EndPos = destination;
+        StartTime = startTime;
+        Duration = duration;
     }
 }
+
